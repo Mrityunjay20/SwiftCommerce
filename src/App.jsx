1b833ea73a9e915f6430a5996ac3bf424be85c68
Mrityunjay20 <mrityunjay.shrivastava20@gmail.com>
2024-04-01 10:19:08 +0530

    landing page

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+  <>
+   
+   <div class="min-h-screen flex user-select-none  items-center justify-center">
+    <a href="#" class="block max-w-sm mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Swift Commerce</h5>
+        <p class="font-normal justify-center text-gray-700">Ecommerce Development made better with our 100% managed commerce backend</p>
+        <p>(Coming Soon)</p>
+    </a>
+</div>
+
+  </>
+
+  )
+}
+
+export default App
